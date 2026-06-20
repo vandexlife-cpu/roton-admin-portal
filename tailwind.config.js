b9a6417f08cbba5fa464b91fc91009c35b9786de
vandexlife-cpu <vandexlife@gmail.com>
2026-06-20 11:23:16 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./RotonAdminPortal.jsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

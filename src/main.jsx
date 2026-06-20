e06f0ea60827ae38da80c8ebd020766b867ff896
vandexlife-cpu <vandexlife@gmail.com>
2026-06-20 10:19:51 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RotonAdminPortal from './RotonAdminPortal.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RotonAdminPortal />
+  </React.StrictMode>,
+)
+
